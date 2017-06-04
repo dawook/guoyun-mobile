5f253abb02f092f75459d48bcb82e7c18a1b7f34
七叶子卿 <15256936283@163.com>
2017-06-04 22:37:52 +0800

    commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-header></v-header>
+    home
+  </div>
+</template>
+
+<script>
+import vHeader from '@/components/header/'
+
+export default {
+   name: 'vHome',
+   components: {
+    vHeader
+   }
+}
+</script>
