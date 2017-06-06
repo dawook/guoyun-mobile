efbaaedfe64c6450970fad6e7446c1e89d63f7e3
七叶子卿 <15256936283@163.com>
2017-06-06 17:18:10 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
     <v-header></v-header>
+    <swipe 
+    	v-model="index" 
+    	style="text-align: center; line-height: 80px; height: 100px;"
+    	:pagination='false'>
+		  <swipe-item style="height: 100px;"><img :src="img"></swipe-item>
+		  <swipe-item style="height: 100px;">item2</swipe-item>
+		  <swipe-item style="height: 100px;">item3</swipe-item>
+		</swipe>
   </div>
 </template>
 
 <script>
 import vHeader from '@/components/header/'
+import { Swipe, SwipeItem } from 'c-swipe';
+require('c-swipe/dist/swipe.css');
+
 export default {
    name: 'vHome',
    components: {
-    vHeader
+    vHeader,
+    Swipe,
+    SwipeItem
+   },
+   data() {
+   	return {
+   		index: 0,
+   		img: require('@/assets/imgs/swiper_01.png')
+   	}
    }
 }
 </script>
