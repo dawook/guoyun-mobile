39904a884ad0c89e35a8b7cf8b6ac625fa6a7cc3
七叶子卿 <15256936283@163.com>
2017-06-06 16:57:05 +0800

    commit

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div :class='objCls'>
-    header
+  	<template v-if='isIndex'>
+  		<router-link :to="{ path: '/' }" class='header__logo'>
+	  		<i class='icon-logo'></i>
+	  	</router-link>
+			<nav class='header__nav'>
+				<router-link :to="{ path: '/' }" v-if='!isLogin'>
+		  		<i class='icon-user'></i>
+		  	</router-link>
+		  	<router-link :to="{ path: '/' }" v-else>
+		  		<img :src="avtarImg" class='header__avtar'>
+		  	</router-link>
+		  	<router-link :to="{ path: '/' }">
+		  		<i class='icon-notice-circle'></i>
+		  	</router-link>
+		  	<router-link :to="{ path: '/' }">
+		  		<i class='icon-setting'></i>
+		  	</router-link>
+			</nav>
+
+  	</template>
+  	
   </div>
 </template>
 
@@ -12,7 +32,13 @@
     props: {
     	isIndex: {
     		type: Boolean,
-    		default: false
+    		default: true
+    	}
+    },
+    data() {
+    	return {
+    		avtarImg: require('@/assets/imgs/avatar.jpg'),
+    		isLogin: false
     	}
     },
     computed: {
