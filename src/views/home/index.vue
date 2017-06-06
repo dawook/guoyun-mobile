8ee7c0787eb6fd104b445c97b3914b44eb518e5f
七叶子卿 <15256936283@163.com>
2017-06-06 17:05:32 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <v-header></v-header>
-    home
   </div>
 </template>
 
 <script>
 import vHeader from '@/components/header/'
-
 export default {
    name: 'vHome',
    components: {
