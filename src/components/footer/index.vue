faa8f6801d1444e9fb3ea769b5710ad754c77a6a
七叶子卿 <15256936283@163.com>
2017-06-08 15:40:51 +0800

    commit

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,6 +1,14 @@
 <template>
-	<footer class='footer b-t-1px'>
-		<p class='copyright-info'>&copy; 2016 - 2017 国运产权</p>
+	<footer class='footer'>
+		<router-link to='/reg'>注册</router-link>
+		<router-link to='/login'>登录</router-link>
+		<router-link to='/help'>帮助</router-link>
+		<router-link to='/risk'>风险提示</router-link>
+		<router-link to='/app'>客户端</router-link>
+		
+		<p class='copyright-info b-t-1px'>&copy; 2016 - 2017 国运产权</p>
+
+		<p class='tips'>市场有风险，投资须谨慎</p>
 	</footer>
 </template>
 <script>
