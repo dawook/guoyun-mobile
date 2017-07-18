d5f8c931bd95e32174f1d70e9e89049ea5e8fa42
七叶子卿 <15256936283@163.com>
2017-07-18 17:25:25 +0800

    commit

---
+++ src/pages/project/pvp.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>film</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,5 +8,25 @@ export default new Router({
 		path: '/',
 		name: 'Home',
 		component: resolve => require(['@/pages/home'], resolve)
+	}, {
+		path: '/pvp',
+		name: 'pvp',
+		component: resolve => require(['@/pages/project/pvp.vue'], resolve)
+	}, {
+		path: '/ppp',
+		name: 'ppp',
+		component: resolve => require(['@/pages/project/ppp.vue'], resolve)
+	}, {
+		path: '/school',
+		name: 'school',
+		component: resolve => require(['@/pages/project/school.vue'], resolve)
+	}, {
+		path: '/film',
+		name: 'film',
+		component: resolve => require(['@/pages/project/film.vue'], resolve)
+	}, {
+		path: '/house',
+		name: 'house',
+		component: resolve => require(['@/pages/project/house.vue'], resolve)
 	}]
 })
